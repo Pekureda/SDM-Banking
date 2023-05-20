9c6b15724cda00aa9a1f4a96bdfeb9e2c13596f1
Paweł Perechuda <aragok3@o2.pl>
2023-05-20 15:11:24 +0200

    Refactored Customer out of Bank

---
+++ BankingLibrary/src/bank/Account.java
@@ -0,0 +1,36 @@
+package bank;
+
+import java.util.Currency;
+
+class Account {
+    private int id;
+    private double balance;
+    private Currency currency;
+    private AccountType type;
+
+    //Deposit deposit = new Deposit();
+
+    public void createTransaction() {
+        // TODO: 18/05/2023
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void createDeposit() {
+        // TODO: 18/05/2023
+    }
+
+    public void createLoan() {
+        // TODO: 18/05/2023
+    }
+
+    public void receiveTransaction() {
+        // TODO: 18/05/2023
+    }
+}
